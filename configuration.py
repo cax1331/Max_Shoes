a812389a34a7a5d6a6cfdcf469b39b4518b9baee
Christian Apodaca <124328199+cax1331@users.noreply.github.com>
2025-06-25 12:54:41 -0700

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,9 @@
+# Base URL of the e-commerce site
+BASE_URL = "https://www.nike.com"
+
+# Browser configuration
+BROWSER = "chrome"
+
+# Timeout values for WebDriverWait (in seconds)
+WAIT_TIMEOUT = 5
+LONG_TIMEOUT = 10
