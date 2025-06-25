79f774cb4a1a7f7db2c85cd613f069320cc7b9dd
Christian Apodaca <124328199+cax1331@users.noreply.github.com>
2025-06-25 12:55:31 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,19 @@
+# Keyword to search on the e-shop
+search_keyword = "Air Max"
+
+# Dummy credit card data for simulation purposes
+card_number = "4242 4242 4242 4242"
+card_expiry = "12/26"
+card_cvc = "123"
+
+# Sample customer contact info
+customer_email = "testuser@example.com"
+customer_phone = "+1 123 456 7890"
+
+# Sample customer address
+customer_address = {
+    "street": "123 Test Ave",
+    "city": "Testville",
+    "zip": "12345",
+    "country": "USA"
+}
